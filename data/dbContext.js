4c5add64535a2c7a0a981bacf909520e6d95426f
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-04-07 13:55:54 -0400

    use dotenv module to load secrets
    Signed-off-by: Harpreet Singh <90693057+devhs89@users.noreply.github.com>

--- data/dbContext.js
+++ data/dbContext.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const _ = require('dotenv').config();
 
 // Mongoose Settings
-mongoose.set({ strictQuery: false });
+mongoose.set({strictQuery: false});
+
+// Get Database Credentials
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
 
 // Connect to MongoDB
-const connStr = `mongodb+srv://${dbUser}:${dbPassword}@cc-prog8670-cluster.kizkzcv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-mongoose.connect(connStr, { useNewUrlParser: true });
+const connStr = `mongodb+srv://${dbUser}:${dbPassword}@main-cluster.z20qngn.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+mongoose.connect(connStr, {useNewUrlParser: true});
